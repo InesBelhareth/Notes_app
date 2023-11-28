abb9a55ae7a9e98d17da1049e880002cbf3cdfe7
Ines <inesbelhareth4@gmail.com>
2023-11-28 23:10:09 +0100

    commit

--- server/database/dbConfig.js
+++ server/database/dbConfig.js
@@ -10,7 +10,7 @@ const MONGO_URI = process.env.MONGO_URI || "mongodb://localhost:27017/Notes";
 
 const connectToDB = async () => {
   try {
-    const connect = await mongoose.connect("MONGO_URI, connOptions");
+    const connect = await mongoose.connect("mongodb://notesapp:7aWCN6zB4KOnHT1IUICVg15DtkIGyVtMnU7G6WInpJ4Hpuzw15JPHG9svH69gODffRv7f27sGlY9ACDbxakUug==@notesapp.mongo.cosmos.azure.com:10255/?ssl=true&replicaSet=globaldb&retrywrites=false&maxIdleTimeMS=120000&appName=@notesapp@");
     if (connect) console.log(`Mongodb connected - ${connect.connection.host}`);
   } catch (err) {
     console.log(`Database error ${err}`);
